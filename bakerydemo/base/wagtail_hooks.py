7579a0a666b7655051cc3305c97ea79d23f4c4b3
Matt Westcott <matt@west.co.tt>
2022-11-28 18:04:54 +0000

    lint

--- bakerydemo/base/wagtail_hooks.py
+++ bakerydemo/base/wagtail_hooks.py
@@ -77,9 +77,10 @@ modeladmin_register(BakeryModelAdminGroup)
 
 
 from wagtail.core import hooks
+
 from bakerydemo.base.views import PersonChooserViewSet
 
 
-@hooks.register('register_admin_viewset')
+@hooks.register("register_admin_viewset")
 def register_person_chooser_viewset():
-    return PersonChooserViewSet('person_chooser', url_prefix='person-chooser')
+    return PersonChooserViewSet("person_chooser", url_prefix="person-chooser")
--- bakerydemo/base/widgets.py
+++ bakerydemo/base/widgets.py
@@ -1,19 +1,18 @@
 from django.contrib.admin.utils import quote
 from django.urls import reverse
 from django.utils.translation import gettext_lazy as _
-
 from generic_chooser.widgets import AdminChooser
 
 from bakerydemo.base.models import Person
 
 
 class PersonChooser(AdminChooser):
-    choose_one_text = _('Choose a person')
-    choose_another_text = _('Choose another person')
-    link_to_chosen_text = _('Edit this person')
+    choose_one_text = _("Choose a person")
+    choose_another_text = _("Choose another person")
+    link_to_chosen_text = _("Edit this person")
     model = Person
-    choose_modal_url_name = 'person_chooser:choose'
-    icon = 'user'
+    choose_modal_url_name = "person_chooser:choose"
+    icon = "user"
 
     def get_edit_item_url(self, item):
-        return reverse('wagtailsnippets_base_person:edit', args=(quote(item.pk), ))
+        return reverse("wagtailsnippets_base_person:edit", args=(quote(item.pk),))
